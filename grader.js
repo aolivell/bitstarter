20715bb080108261e4c06ca2f93afe72e52c8dc7
Andres Olivella <aolivell@gmail.com>
2013-07-10 06:40:30 +0000

    completed grader.js with 1st two tags

--- grader.js
+++ grader.js
@@ -4,35 +4,38 @@ var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
-var CHEKSFILE_DEFAULT = "checks.json";
+var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
   var instr = infile.toString();
   if (!fs.existsSync(instr)) {
     console.log("%s does not exist. Exiting.", instr);
-    process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    process.exit(1);
   }
   return instr;
 };
 
 var cheerioHtmlFile = function(htmlfile) {
-  return cheerio.load(fs.readFileSync(htmlfile));
+  var html = fs.readFileSync(htmlfile).toString();
+  return cheerio.load(html);
 };
 
 var loadChecks = function(checksfile) {
   return JSON.parse(fs.readFileSync(checksfile));
 };
 
+
 var checkHtmlFile = function(htmlfile, checksfile) {
-  $ = cheerioHtmlFile(htmlfile);
+  var $ = cheerioHtmlFile(htmlfile);
   var checks = loadChecks(checksfile).sort();
   var out = {};
   for(var ii in checks) {
-    var present = $(checks[ii]).lenght > 0;
-    out[checks[ii]] = presnet;
+    var present = $(checks[ii]).length > 0;
+    out[checks[ii]] = present;
   }
   return out;
 };
+
 
 var clone = function(fn) {
   // Workaroud for commader.js issue.
